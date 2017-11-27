ab0ecc99060d3f1c86092ff5be94b37e2def913b
Lane Little <llittle3701@live.com>
2017-11-26 21:46:47 -0600

    Updated Client.c

--- Client.c
+++ Client.c
@@ -1,5 +1,5 @@
 /*
-** client.c -- a stream socket client demo
+** Client.c -- Lab3
 */
 
 #include <stdio.h>
@@ -14,7 +14,7 @@
 
 #include <arpa/inet.h>
 
-#define PORT "10010" // the port client will be connecting to 
+//#define PORT "10010" // the port client will be connecting to 
 
 #define MAXDATASIZE 100 // max number of bytes we can get at once 
 
@@ -37,15 +37,19 @@ int main(int argc, char *argv[])
 	char s[INET6_ADDRSTRLEN];
 
 	if (argc != 2) {
-	    fprintf(stderr,"usage: client hostname\n");
+	    fprintf(stderr,"usage: Client ServerName ServerPort MyPort\n");
 	    exit(1);
 	}
 
 	memset(&hints, 0, sizeof hints);
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
+	
+	char[] serverName = argsv[1];
+	int portNum = atoi(argsv[2]);
+	int myPort = atoi(argsv[3]);
 
-	if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
+	if ((rv = getaddrinfo(argv[1], portNum, &hints, &servinfo)) != 0) {
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
 		return 1;
 	}
